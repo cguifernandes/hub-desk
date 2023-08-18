6a86141131b88702b8c7fab7e1d002b4f6c0771d
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-08-17 22:23:15 -0300

    style(messages): adding framer motion to cookie message

--- src/components/messages/cookies.tsx
+++ src/components/messages/cookies.tsx
@@ -1,36 +1,56 @@
+'use client'
 import { X } from 'lucide-react'
 import Button from '../button'
 import Text from '../text'
 import clsx from 'clsx'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 /* eslint-disable react/no-unescaped-entities */
 const CookiesMessage = () => {
+  const [visible, setVisible] = useState(true)
+
   return (
-    <div
-      className={clsx(
-        'fixed bottom-0 right-0 m-8 flex max-w-[320px] flex-col items-center md:bottom-8 md:right-8',
-        'justify-between rounded-lg bg-grey-600 p-10 pt-12 md:w-[690px] md:flex-row md:px-10 md:py-4',
+    <>
+      {visible && (
+        <motion.div
+          initial={{ opacity: 0, translateY: 20 }}
+          animate={{ opacity: 1, translateY: 0 }}
+          transition={{ type: 'keyframes' }}
+          className={clsx(
+            'fixed bottom-8 right-8 flex w-[300px] flex-col items-center',
+            'justify-between rounded-lg bg-grey-600 px-10 py-8 md:w-[490px]',
+          )}
+        >
+          <button className="group absolute right-4 top-4">
+            <X
+              onClick={() => setVisible(!visible)}
+              className="text-white transition-colors group-hover:text-white/60"
+              size={32}
+            />
+          </button>
+          <Text align="center" className="p-6 text-sm">
+            Utilizamos cookies para aprimorar a sua experiência de navegação,
+            oferecer anúncios ou conteúdo personalizado e analisar o nosso
+            tráfego. Ao clicar em "Aceitar todos", você consente com o nosso uso
+            de cookies.
+          </Text>
+          <div className="flex flex-col justify-evenly gap-y-8">
+            <Button
+              onClick={() => setVisible(!visible)}
+              text="Aceitar todos"
+              className="w-52 text-sm"
+            />
+            <Button
+              onClick={() => setVisible(!visible)}
+              text="Negador todos"
+              fill="empty"
+              className="w-52 text-sm"
+            />
+          </div>
+        </motion.div>
       )}
-    >
-      <button className="group absolute right-4 top-4">
-        <X
-          className="text-white transition-colors group-hover:text-white/60"
-          size={32}
-        />
-      </button>
-      <Text
-        align="center"
-        className="pb-6 text-lg md:p-8 md:pl-0 md:!text-left"
-      >
-        Utilizamos cookies para aprimorar a sua experiência de navegação,
-        oferecer anúncios ou conteúdo personalizado e analisar o nosso tráfego.
-        Ao clicar em "Aceitar todos", você consente com o nosso uso de cookies.
-      </Text>
-      <div className="flex flex-col justify-evenly gap-y-8">
-        <Button text="Aceitar todos" className="w-52" />
-        <Button text="Negador todos" fill="empty" className="w-52" />
-      </div>
-    </div>
+    </>
   )
 }
 
