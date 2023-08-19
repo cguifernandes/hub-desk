9387b333ebbe495177de74f83f83045308d14080
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-08-19 13:23:33 -0300

    feat(auth): finish front-end for sign-in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,12 @@
 import Heading from '@/components/heading'
 import Text from '@/components/text'
-import Image from 'next/image'
-import LogoImage from '../../../../public/logoImage.svg'
 import { Metadata } from 'next'
 import { Form } from '@/components/form'
+import { Eye, Facebook, Github, Mail } from 'lucide-react'
+import clsx from 'clsx'
+import Google from '../../../../public/googleLogo.svg'
+import Linkedin from '../../../../public/linkedinLogo.svg'
+import Image from 'next/image'
 
 export const metadata: Metadata = {
   title: 'Logar | Hub Desk',
@@ -25,19 +28,45 @@ const SignIn = () => {
             </a>
           </Text>
         </div>
-        <Image
-          quality={100}
-          width={52}
-          height={52}
-          alt="Logo imagem"
-          src={LogoImage}
-        />
       </header>
-      <Form.Root className="space-y-8 pt-12">
-        <Form.Input placeholder="E-mail" />
-        <Form.Input placeholder="Senha" />
+      <Form.Root className="space-y-8 py-12">
+        <Form.Input className="relative" placeholder="E-mail">
+          <div
+            className={clsx(
+              'absolute right-0 top-[50%] flex w-[60px] justify-center',
+              ' h-[60px] -translate-y-[50%] items-center rounded-r-md bg-sky-700',
+            )}
+          >
+            <Mail color="#fff" strokeWidth={1.25} size={28} />
+          </div>
+        </Form.Input>
+        <Form.Input placeholder="Senha">
+          <div
+            className={clsx(
+              'absolute right-0 top-[50%] flex w-[60px] justify-center bg-sky-700',
+              ' h-[60px] -translate-y-[50%] items-center rounded-r-md',
+            )}
+          >
+            <Eye color="#fff" strokeWidth={1.25} size={28} />
+          </div>
+        </Form.Input>
         <Form.Button className="w-full" text="Logar" />
       </Form.Root>
+      <div className="h-[2px] w-full bg-grey-400" />
+      <div className="flex justify-around pt-12">
+        <button className="flex h-16 w-24 items-center justify-center rounded-md bg-black transition-colors hover:bg-[#202020]">
+          <Github fill="#fff" strokeWidth={0.5} color="#fff" size={28} />
+        </button>
+        <button className="flex h-16 w-24 items-center justify-center rounded-md bg-[#176DF0] transition-colors hover:bg-[#176ef0ab]">
+          <Facebook fill="#fff" strokeWidth={0.5} color="#fff" size={28} />
+        </button>
+        <button className="flex h-16 w-24 items-center justify-center rounded-md bg-[#0e76a8] transition-colors hover:bg-[#0e77a8c9]">
+          <Image src={Linkedin} alt="Google logo" width={28} />
+        </button>
+        <button className="flex h-16 w-24 items-center justify-center rounded-md bg-white transition-colors hover:bg-[#ddd]">
+          <Image src={Google} alt="Google logo" width={28} />
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,15 +1,33 @@
 import clsx from 'clsx'
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, ReactNode } from 'react'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   className?: string
+  children?: ReactNode
 }
 
-const FormInput = ({ className, ...props }: InputProps) => {
+const FormInput = ({ className, children, ...props }: InputProps) => {
+  if (children) {
+    return (
+      <div className="relative flex justify-between">
+        <input
+          className={clsx(
+            'w-[calc(520px_-_60px)] rounded-l-md border-2 border-transparent bg-grey-400/30 p-4 ',
+            'text-white placeholder-white/50 transition-colors focus:border-sky-700',
+            className,
+          )}
+          {...props}
+        />
+        {children}
+      </div>
+    )
+  }
+
   return (
     <input
       className={clsx(
-        'w-full rounded-md bg-grey-400/30 p-4 text-white placeholder-white/50',
+        'w-full rounded-l-md border-2 border-transparent bg-grey-400/30 p-4 text-white',
+        'placeholder-white/50 transition-colors focus:border-sky-700',
         className,
       )}
       {...props}
