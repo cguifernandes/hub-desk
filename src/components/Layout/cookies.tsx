61d614902833e371a3cdb919f9879b6904b12f7a
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-08-23 17:26:56 -0300

    feat: finish cookies message

--- src/components/Layout/cookies.tsx
+++ src/components/Layout/cookies.tsx
@@ -5,50 +5,64 @@ import Text from '@/components/text'
 import clsx from 'clsx'
 import { useState } from 'react'
 import { motion } from 'framer-motion'
+import useConnection from '@/hooks/useConnection'
+import { destroyCookie } from 'nookies'
 
 /* eslint-disable react/no-unescaped-entities */
 const CookiesMessage = () => {
   const [visible, setVisible] = useState(true)
+  const { isConnected } = useConnection()
+
+  const disagreeCookies = () => {
+    setVisible(false)
+    destroyCookie(null, 'user_session', {
+      path: '/',
+    })
+  }
 
   return (
     <>
-      {visible && (
-        <motion.div
-          initial={{ opacity: 0, translateY: 20 }}
-          animate={{ opacity: 1, translateY: 0 }}
-          transition={{ type: 'keyframes' }}
-          className={clsx(
-            'fixed bottom-8 right-8 flex w-[300px] flex-col items-center',
-            'justify-between rounded-lg bg-grey-600 px-10 py-8 md:w-[520px]',
+      {isConnected && (
+        <>
+          {visible && (
+            <motion.div
+              initial={{ opacity: 0, translateY: 20 }}
+              animate={{ opacity: 1, translateY: 0 }}
+              transition={{ type: 'keyframes' }}
+              className={clsx(
+                'fixed bottom-8 right-8 flex w-[300px] flex-col items-center',
+                'justify-between rounded-lg bg-grey-600 px-10 py-8 md:w-[520px]',
+              )}
+            >
+              <button className="group absolute right-4 top-4">
+                <X
+                  onClick={() => setVisible(!visible)}
+                  className="text-white transition-colors group-hover:text-white/60"
+                  size={32}
+                />
+              </button>
+              <Text align="center" className="p-6 text-sm">
+                Utilizamos cookies para aprimorar a sua experiência de
+                navegação, oferecer anúncios ou conteúdo personalizado e
+                analisar o nosso tráfego. Ao clicar em "Aceitar todos", você
+                consente com o nosso uso de cookies.
+              </Text>
+              <div className="flex w-full flex-col justify-between gap-y-8 md:flex-row">
+                <Button
+                  onClick={() => setVisible(false)}
+                  text="Aceitar todos"
+                  className="w-52 text-sm"
+                />
+                <Button
+                  onClick={disagreeCookies}
+                  text="Negador todos"
+                  fill="empty"
+                  className="w-52 text-sm"
+                />
+              </div>
+            </motion.div>
           )}
-        >
-          <button className="group absolute right-4 top-4">
-            <X
-              onClick={() => setVisible(!visible)}
-              className="text-white transition-colors group-hover:text-white/60"
-              size={32}
-            />
-          </button>
-          <Text align="center" className="p-6 text-sm">
-            Utilizamos cookies para aprimorar a sua experiência de navegação,
-            oferecer anúncios ou conteúdo personalizado e analisar o nosso
-            tráfego. Ao clicar em "Aceitar todos", você consente com o nosso uso
-            de cookies.
-          </Text>
-          <div className="flex w-full flex-col justify-between gap-y-8 md:flex-row">
-            <Button
-              onClick={() => setVisible(!visible)}
-              text="Aceitar todos"
-              className="w-52 text-sm"
-            />
-            <Button
-              onClick={() => setVisible(!visible)}
-              text="Negador todos"
-              fill="empty"
-              className="w-52 text-sm"
-            />
-          </div>
-        </motion.div>
+        </>
       )}
     </>
   )
---
+++ src/hooks/useConnection.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import { parseCookies } from 'nookies'
+import { useEffect, useState } from 'react'
+
+const useConnection = () => {
+  const { user_session } = parseCookies()
+  const [cookies, setCookies] = useState({})
+  const isConnected = !!user_session
+
+  useEffect(() => {
+    if (user_session) {
+      setCookies(user_session)
+    }
+  }, [user_session])
+
+  return { cookies, isConnected }
+}
+
+export default useConnection
