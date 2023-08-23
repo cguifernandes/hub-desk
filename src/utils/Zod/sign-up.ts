40e5b27fcc5cab5312a53a865b04620dd291336d
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-08-22 21:19:28 -0300

    feat: adjusting zod

--- src/utils/Zod/sign-up.ts
+++ src/utils/Zod/sign-up.ts
@@ -1,16 +1,7 @@
 import { z } from 'zod'
 
 export const schemaSignUp = z.object({
-  name: z
-    .string()
-    .nonempty('O campo "User" é obrigatório.')
-    .toLowerCase()
-    .max(16, 'O user deve ter no máximo 16 caracteres')
-    .refine(
-      (user) =>
-        /^[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(user.charAt(0)) === false,
-      'O user não deve começar com um caractere especial.',
-    ),
+  name: z.string().nonempty('O campo "Nome" é obrigatório.'),
   email: z
     .string()
     .email('O e-mail precisa ser válido.')
