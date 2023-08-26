145c365d19ead749c7b98d418070f734830e6ea1
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-08-26 15:26:43 -0300

    feat: adjusting modal user

--- src/components/Client/modal.tsx
+++ src/components/Client/modal.tsx
@@ -8,9 +8,11 @@ import Topics from '../topics'
 import Button from '../button'
 import { LogOut, Settings } from 'lucide-react'
 import Line from '../line'
+import useConnection from '@/hooks/useConnection'
 
 const ModalBar = ({ id, name, pfp }: ClientsProps) => {
   const [visibleModal, setVisibleModal] = useState(false)
+  const { isConnected } = useConnection()
 
   return (
     <>
@@ -33,23 +35,25 @@ const ModalBar = ({ id, name, pfp }: ClientsProps) => {
           <Modal.Overlay onClick={() => setVisibleModal(false)} />
           <Modal.Children className="z-20 m-10 mt-28 max-h-[calc(90%_-_92px)] w-[380px] overflow-y-auto rounded-md bg-grey-600 p-10 shadow-lg sm:w-[450px]">
             <Modal.Header>
-              <div className="flex flex-col items-center justify-between gap-y-8">
-                <Image
-                  alt={name}
-                  src={pfp}
-                  width={80}
-                  height={80}
-                  quality={100}
-                  className="rounded-full"
-                />
-                <Heading
-                  size="md"
-                  className="w-full overflow-hidden text-ellipsis whitespace-nowrap"
-                  align="center"
-                >
-                  {name}
-                </Heading>
-              </div>
+              {isConnected && (
+                <div className="flex flex-col items-center justify-between gap-y-8">
+                  <Image
+                    alt={name}
+                    src={pfp}
+                    width={80}
+                    height={80}
+                    quality={100}
+                    className="rounded-full"
+                  />
+                  <Heading
+                    size="md"
+                    className="w-full overflow-hidden text-ellipsis whitespace-nowrap"
+                    align="center"
+                  >
+                    {name}
+                  </Heading>
+                </div>
+              )}
             </Modal.Header>
             <Line className="my-8" />
             <div className="grid grid-cols-1 grid-rows-3 gap-5 sm:grid-cols-2">
