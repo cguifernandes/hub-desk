99d3d0307d594137f0b4062961a3a546c6296f9a
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-09-02 00:28:57 -0300

    feat: adjusting modal

--- src/components/Modal/ModalOverlay.tsx
+++ src/components/Modal/ModalOverlay.tsx
@@ -1,26 +1,13 @@
 'use client'
-import { Variants, motion } from 'framer-motion'
-
 type ModalOverlay = {
   onClick: () => void
   visibleModal: boolean
 }
 
 const ModalOverlay = ({ onClick, visibleModal }: ModalOverlay) => {
-  const animationModal: Variants = {
-    visible: {
-      opacity: 1,
-    },
-    hidden: {
-      opacity: 0,
-    },
-  }
-
   return (
-    <motion.div
-      animate={visibleModal ? 'visible' : 'hidden'}
-      transition={{ type: 'keyframes' }}
-      variants={animationModal}
+    <div
+      style={visibleModal ? { opacity: 1 } : { opacity: 0 }}
       onClick={onClick}
       className="fixed left-0 top-0 z-10 h-screen w-screen bg-grey-700/30 backdrop-blur-sm"
     />
