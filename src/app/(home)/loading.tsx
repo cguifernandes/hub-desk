98c7bb52d84e7f9b68c070097a37d724c0ed3d76
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-09-04 09:01:17 -0300

    style: adjusting skeleton loading

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -4,8 +4,8 @@ const Loading = () => {
   return (
     <div className="mt-24">
       <section className="flex h-[calc(100vh_-_96px)] items-center justify-center bg-gradient-to-b from-grey-550 to-grey-500">
-        <div className="flex w-[90%] flex-col justify-center gap-y-4">
-          <Skeleton className="m-auto w-2/12" height={32} />
+        <div className="w-[90%] space-y-4 sm:w-[80%] lg:w-[920px] xl:w-[1020px]">
+          <Skeleton className="m-auto" width={240} height={32} />
           <Skeleton className="w-full" height={144} />
           <Skeleton className="m-auto w-6/12" height={48} />
         </div>
