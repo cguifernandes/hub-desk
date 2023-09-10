f5b4999f910c1a7918837ec6b3b9e95e1295e430
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-09-10 01:52:03 -0300

    style: adjusting loading desk page

--- src/app/(layout)/(private-routes)/desk/loading.tsx
+++ src/app/(layout)/(private-routes)/desk/loading.tsx
@@ -2,22 +2,18 @@ import Skeleton from '@/components/skeleton'
 
 const Loading = () => {
   return (
-    <main className="pt-24">
-      <section className="flex min-h-[calc(100vh_-_192px)] flex-col items-center bg-gradient-to-b from-grey-550 to-grey-500">
-        <div className="w-[90%] space-y-2 py-16 text-center sm:w-[80%] lg:w-[920px] xl:w-[1020px]">
-          <Skeleton className="m-auto w-10/12" height={28} />
-          <Skeleton className="m-auto w-4/12" height={24} />
-        </div>
-        <div className="flex w-full items-center justify-around py-12">
-          <div className="w-11/12 space-y-8 sm:w-8/12 lg:w-5/12">
-            <Skeleton height={60} className="w-full" />
-            <Skeleton height={60} className="w-full" />
-            <Skeleton height={192} className="w-full" />
-            <Skeleton height={48} className="w-full" />
-          </div>
-        </div>
-      </section>
-    </main>
+    <section className="flex min-h-[calc(100vh_-_192px)] flex-col items-center bg-gradient-to-b from-grey-550 to-grey-500">
+      <div className="w-[90%] space-y-2 py-16 text-center sm:w-[80%] lg:w-[920px] xl:w-[1020px]">
+        <Skeleton className="m-auto w-10/12" height={28} />
+        <Skeleton className="m-auto w-4/12" height={24} />
+      </div>
+      <div className="w-11/12 space-y-8 sm:w-8/12 lg:w-5/12">
+        <Skeleton height={60} className="w-full" />
+        <Skeleton height={60} className="w-full" />
+        <Skeleton height={192} className="w-full" />
+        <Skeleton height={48} className="w-full" />
+      </div>
+    </section>
   )
 }
 
