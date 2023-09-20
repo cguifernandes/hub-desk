a808ab946778b94857cc078e77080b266fd090fc
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-09-20 11:36:56 -0300

    fix: adjusting profile picture

--- src/components/Layout/header.tsx
+++ src/components/Layout/header.tsx
@@ -1,4 +1,4 @@
-import { Profile } from '@/app/server-components'
+import Nav from '../Client/nav'
 import Back from '../back'
 
 const Header = () => {
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="fixed top-0 z-50 flex h-24 w-full justify-center bg-grey-500 backdrop-blur md:bg-grey-500/60">
       <div className="mx-6 flex w-full max-w-[1636px] items-center justify-between ">
         <Back href="/" isHeader className="w-[230px] sm:w-[280px]" />
-        <Profile />
+        <Nav />
       </div>
     </header>
   )
