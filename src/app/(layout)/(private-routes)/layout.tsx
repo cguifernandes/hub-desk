1d3020684ed70d18f14b391660aa45e7275f82ab
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-09-20 12:17:47 -0300

    feat: adjusting skeleton

--- src/app/(layout)/(private-routes)/layout.tsx
+++ src/app/(layout)/(private-routes)/layout.tsx
@@ -3,8 +3,8 @@ import { ReactNode, useEffect, useState } from 'react'
 import { ROUTES } from '@/utils/constant'
 import { useRouter } from 'next/navigation'
 import RedirectWrapper from '@/components/Layout/redirect'
-import Loading from '@/components/Layout/loading'
 import useClient from '@/hooks/useClient'
+import Skeleton from '@/components/Layout/skeleton'
 
 type LayoutProps = {
   children: ReactNode
@@ -31,9 +31,15 @@ const Layout = ({ children }: LayoutProps) => {
     <>
       <section className="flex min-h-[calc(100vh_-_192px)] flex-col items-center bg-gradient-to-b from-grey-550 to-grey-500">
         {!mounted ? (
-          <div className="flex h-[calc(100vh_-_192px)] items-center justify-center">
-            <Loading className="h-20 w-20" />
-          </div>
+          <>
+            <div className="flex w-[90%] flex-col items-center justify-center gap-y-1 py-16 text-center sm:w-[80%] lg:w-[920px] xl:w-[1020px]">
+              <Skeleton className="w-full" height={32} />
+              <Skeleton className="w-6/12" height={20} />
+            </div>
+            <div className="flex w-full grow-[1] basis-0 flex-wrap justify-center gap-8 px-6 py-4">
+              <Skeleton className="h-[590px] max-w-[450px] flex-1" />
+            </div>
+          </>
         ) : !isConnected ? (
           <RedirectWrapper
             text="Parece que você não está cadastrado(a)!"
