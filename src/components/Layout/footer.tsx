b51b0f708b34bc5dc441ddb94cf5a44d82005ba2
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-11-27 16:44:06 -0300

    style: adjusting size text on footer

--- src/components/Layout/footer.tsx
+++ src/components/Layout/footer.tsx
@@ -6,26 +6,22 @@ const Footer = () => {
   return (
     <footer
       className={clsx(
-        'flex h-auto flex-col items-center justify-center gap-2 ',
-        'px-[60px] py-6 text-center md:h-24 md:flex-row md:justify-between',
+        'flex h-auto flex-col items-center justify-center gap-2 bg-grey-500/30',
+        'px-12 py-6 text-center md:h-16 md:flex-row md:justify-between md:py-0',
       )}
     >
-      <div>
-        {/*  TODO: Adicionar páginas */}
-        <Text size="md">Hub Desk 2023 ©</Text>
-      </div>
-      <div>
-        <Text>
-          Desenvolvido por{' '}
-          <Link
-            target="_blank"
-            className="transition-colors hover:text-grey-200"
-            href="https://guifernandes.vercel.app"
-          >
-            Guilherme Fernandes
-          </Link>
-        </Text>
-      </div>
+      {/*  TODO: Adicionar páginas */}
+      <Text className="text-base">Hub Desk 2023 ©</Text>
+      <Text className="text-sm">
+        Desenvolvido por{' '}
+        <Link
+          target="_blank"
+          className="gradient-text"
+          href="https://guifernandes.vercel.app"
+        >
+          Guilherme Fernandes
+        </Link>
+      </Text>
     </footer>
   )
 }
