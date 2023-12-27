2e56969dd0b4779f9b0cd1bf716976fbe1236e11
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-12-27 09:56:45 -0300

    feat: adding verification if a member has invite

--- src/app/api/invite/route.ts
+++ src/app/api/invite/route.ts
@@ -4,6 +4,7 @@ import prisma from '../../../../lib/prisma'
 export async function POST(request: NextRequest) {
   const body = await request.json()
   const { deskId, receiverId, senderId } = body
+
   try {
     if (!senderId) {
       return NextResponse.json({
@@ -11,23 +12,36 @@ export async function POST(request: NextRequest) {
       })
     }
 
-    try {
-      await prisma.invite.create({
-        data: {
-          senderId,
-          deskId,
-          receiverId,
-        },
-      })
+    const hasInvite = await prisma.invite.findFirst({
+      where: {
+        receiverId,
+        deskId,
+      },
+    })
 
+    if (hasInvite) {
       return NextResponse.json({
-        success: 'Convite enviado com sucesso.',
+        error: 'Esse usuário já foi convidado para essa desk.',
       })
-    } catch (error) {
-      console.log(error)
-      return NextResponse.json({
-        error,
-      })
+    } else {
+      try {
+        await prisma.invite.create({
+          data: {
+            senderId,
+            deskId,
+            receiverId,
+          },
+        })
+
+        return NextResponse.json({
+          success: 'Convite enviado com sucesso.',
+        })
+      } catch (error) {
+        console.log(error)
+        return NextResponse.json({
+          error,
+        })
+      }
     }
   } catch (error) {
     throw new Error(JSON.stringify(error))
